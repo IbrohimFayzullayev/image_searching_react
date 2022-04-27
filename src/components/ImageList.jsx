2860d058dc81d3523305b61dc54310ddf7c6c3da
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-27 08:42:05 +0500

    project

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "../StyleImage.css";
+import ImageCard from "./ImageCard";
 
-const ImageList = () => {
-  const onClickBtn = () => {
-    console.log("Meni bosding");
+const ImageList = (props) => {
+  // const arrs = props.dataImg.map((val) => val);
+  // console.log(arrs);
+  const imageRender = () => {
+    return props.dataImg.map((val) => {
+      return <ImageCard key={val.id} data={val} />;
+    });
   };
-  return <button onClick={onClickBtn}>Click Me !</button>;
+  return <div className="img-list">{imageRender()}</div>;
 };
 export default ImageList;
